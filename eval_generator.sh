652dcde36adfeb3be24e565f0a9dff1f51156acc
Bam4d <chrisbam4d@gmail.com>
2022-07-11 09:59:22 +0100

    fixing evals

---
+++ eval_generator.sh
@@ -0,0 +1,21 @@
+python eval.py --levels human --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__0__1657454744
+python eval.py --levels human --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__1__1657454744
+python eval.py --levels human --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__2__1657454744
+python eval.py --levels human --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__3__1657454744
+python eval.py --levels human --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__4__1657454744
+python eval.py --levels human --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__5__1657454744
+python eval.py --levels human --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__6__1657454744
+python eval.py --levels human --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__7__1657454744
+python eval.py --levels human --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__8__1657454744
+python eval.py --levels human --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__9__1657454744
+
+python eval.py --levels generator --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__0__1657454744
+python eval.py --levels generator --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__1__1657454744
+python eval.py --levels generator --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__2__1657454744
+python eval.py --levels generator --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__3__1657454744
+python eval.py --levels generator --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__4__1657454744
+python eval.py --levels generator --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__5__1657454744
+python eval.py --levels generator --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__6__1657454744
+python eval.py --levels generator --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__7__1657454744
+python eval.py --levels generator --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__8__1657454744
+python eval.py --levels generator --seed 48 --checkpoint-dir checkpoints/Grafter_30x30__Default__escape-rooms-final-poolingvec__9__1657454744
